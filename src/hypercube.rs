551eaa37d6a42117e7f5ef898fdf92c6fd1555e1
Andrei Ivasko <andrei@fantom.foundation>
2024-09-13 12:49:05 +0300

    hypercube tests

--- src/hypercube.rs
+++ src/hypercube.rs
@@ -54,3 +54,61 @@ impl<FF: Field> Iterator for HyperCube<FF> {
         Some(result)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_ff::{Zero, One};
+    use ark_bn254::Fr; // Use Fr from the ark_bn254 crate, which is a predefined finite field.
+
+    #[test]
+    fn test_hypercube_initialization() {
+        let max_value = Fr::from(5u64);
+        let hypercube = HyperCube::new(3, max_value);
+
+        assert_eq!(hypercube.current, vec![Fr::zero(); 3]);
+        assert!(!hypercube.ended);
+        assert_eq!(hypercube.max_value, max_value);
+    }
+
+    #[test]
+    fn test_hypercube_iteration() {
+        let max_value = Fr::from(2u64);
+        let mut hypercube = HyperCube::new(2, max_value);
+
+        // First step should be [0, 0]
+        assert_eq!(hypercube.next(), Some(vec![Fr::zero(), Fr::zero()]));
+
+        // Next should be [1, 0]
+        assert_eq!(hypercube.next(), Some(vec![Fr::one(), Fr::zero()]));
+
+        // Next should be [2, 0]
+        assert_eq!(hypercube.next(), Some(vec![Fr::from(2u64), Fr::zero()]));
+
+        // Next should be [0, 1]
+        assert_eq!(hypercube.next(), Some(vec![Fr::zero(), Fr::one()]));
+
+        // Next should be [1, 1]
+        assert_eq!(hypercube.next(), Some(vec![Fr::one(), Fr::one()]));
+
+        // Continue this pattern...
+        assert_eq!(hypercube.next(), Some(vec![Fr::from(2u64), Fr::one()]));
+        assert_eq!(hypercube.next(), Some(vec![Fr::zero(), Fr::from(2u64)]));
+        assert_eq!(hypercube.next(), Some(vec![Fr::one(), Fr::from(2u64)]));
+        assert_eq!(hypercube.next(), Some(vec![Fr::from(2u64), Fr::from(2u64)]));
+
+        // After reaching the max values, it should return None
+        assert_eq!(hypercube.next(), None);
+    }
+
+    #[test]
+    fn test_hypercube_completed_iteration() {
+        let max_value = Fr::from(1u64);
+        let mut hypercube = HyperCube::new(1, max_value);
+
+        // Expecting two iterations [0], [1], then None
+        assert_eq!(hypercube.next(), Some(vec![Fr::zero()]));
+        assert_eq!(hypercube.next(), Some(vec![Fr::one()]));
+        assert_eq!(hypercube.next(), None);
+    }
+}
